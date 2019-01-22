f9f33057f5d5bcb619b5576c5ec4f0ee566fc4e8
Tomasz Cholewa <tomasz@cloudowski.com>
2019-01-22 22:35:43 +0100

    Added socat required by helm

--- ec2_user_data.sh
+++ ec2_user_data.sh
@@ -4,4 +4,4 @@ curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.13
 
 curl -Lo /usr/local/bin/minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x /usr/local/bin/minikube
 
-apt update && apt install -y docker.io python-minimal
+apt update && apt install -y docker.io python-minimal socat
